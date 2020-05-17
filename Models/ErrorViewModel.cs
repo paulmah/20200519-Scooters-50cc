9d38d5fbedad33500f5b39db5d61c402d3e1af92
paulmah <60945152+paulmah@users.noreply.github.com>
2020-05-16 18:30:03 -0600

    first scooters-50cc backup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Scooters_50cc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
